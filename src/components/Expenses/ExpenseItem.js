506ea39f62e73df9d9d2b5711a20a2068a114486
rinoojnr <rinoojnr@gmail.com>
2024-02-29 15:13:50 +0530

    Added state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';  
@@ -5,17 +6,23 @@ import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) =>{
-    console.log(props)
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+      };
+    
     return ( 
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDetails title={props.title} />
+            <ExpenseDetails title={title} />
             <div className="expense-item__price">
                 <div><h2>${props.amount}</h2></div>
             </div>
             <div className="expense-item__location">
-                <h2>{props.LocationOfExpenditure}</h2>
+                <h2>{props.LocationOfExpenditure}</h2>    
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
