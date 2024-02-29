b469e0b69bd2cf7b36f6df42456747dc856788a5
rinoojnr <rinoojnr@gmail.com>
2024-02-29 15:19:33 +0530

    Addedchange amount button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,6 +7,11 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) =>{
     const [title, setTitle] = useState(props.title);
+    const [amount,setAmount] = useState(props.amount);
+
+    const clickHandler1 = () =>{
+        setAmount(100);
+    }
 
     const clickHandler = () => {
         setTitle('Updated!');
@@ -17,12 +22,13 @@ const ExpenseItem = (props) =>{
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={title} />
             <div className="expense-item__price">
-                <div><h2>${props.amount}</h2></div>
+                <div><h2>${amount}</h2></div>
             </div>
             <div className="expense-item__location">
                 <h2>{props.LocationOfExpenditure}</h2>    
             </div>
             <button onClick={clickHandler}>Change Title</button>
+            <button onClick={clickHandler1}>Change Amount</button>
         </Card>
     )
 }
