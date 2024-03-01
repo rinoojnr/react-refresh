bb72e6f0081fb1e36d97ab24f01df2f1f393cd21
rinoojnr <rinoojnr@gmail.com>
2024-03-01 23:25:37 +0530

    Added Dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
+import { useState } from 'react';
 import Expense from './components/Expenses/Expense';
 import ExpenseForm from './components/Expenses/ExpenseForm';
 
+const DUMMY_DATA = [
+  { 
+    id: 'e1',
+    title: 'car insurance', 
+    amount: 849,
+    date: new Date(2024, 1, 26),
+    LocationOfExpenditure: 'Bangalore'
+  },
+  { 
+    id: 'e2',
+    title: 'bike insurance', 
+    amount: 895,
+    date: new Date(2021, 1, 26),
+    LocationOfExpenditure: 'Africa'
+  },
+  { 
+    id: 'e3',
+    title: 'Bike', 
+    amount: 8935,
+    date: new Date(2026, 1, 26),
+    LocationOfExpenditure: 'Kerala'
+  },
+  { 
+    id: 'e4',
+    title: 'Grocerry', 
+    amount: 45,
+    date: new Date(2024, 1, 26),
+    LocationOfExpenditure: 'Calicut'
+  }
+];
+
 function App() {
-  const expenses = [
-    { 
-      id: 'e1',
-      title: 'car insurance', 
-      amount: 849,
-      date: new Date(2024, 1, 26),
-      LocationOfExpenditure: 'Bangalore'
-    },
-    { 
-      id: 'e2',
-      title: 'bike insurance', 
-      amount: 895,
-      date: new Date(2021, 1, 26),
-      LocationOfExpenditure: 'Africa'
-    },
-    { 
-      id: 'e3',
-      title: 'Bike', 
-      amount: 8935,
-      date: new Date(2026, 1, 26),
-      LocationOfExpenditure: 'Kerala'
-    },
-    { 
-      id: 'e4',
-      title: 'Grocerry', 
-      amount: 45,
-      date: new Date(2024, 1, 26),
-      LocationOfExpenditure: 'Calicut'
-    }
-  ];
-  // const arr = [];
-  // for(let i=0;i<expenses.length;i++){
-  //       arr.push(<Expense item={expenses} />)
-  // }
+
+  const [expenses,setExpenses] = useState(DUMMY_DATA);
+
   const saveExpenseDataHandler = (enteredExpenseData) =>{
-      const expenseData = {
-        ...enteredExpenseData,
-        id: Math.random().toString()
-      }
-      expenses.push(expenseData)
-      console.log(expenses,"....")
+    console.log(enteredExpenseData)
+      setExpenses((previousExpense)=>{
+        return [...previousExpense,enteredExpenseData]
+      })
   }
   return (
     <div className="App">
       <h2>Let's get started</h2>
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
       <div><Expense items={expenses} /></div> 
-      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} LocationOfExpenditure={expenses[0].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} LocationOfExpenditure={expenses[1].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} LocationOfExpenditure={expenses[2].LocationOfExpenditure}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} LocationOfExpenditure={expenses[3].LocationOfExpenditure}></ExpenseItem> */}
     </div>
   );
 }
--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -8,17 +8,24 @@ import { useState } from 'react';
 const Expense = (props) =>{
     const [filteredYear,setFilteredYear] = useState('2020')
 
-    const filterChangeHandler = (selectedYear) =>{
+    const filterChangeHandler = (selectedYear) =>{  
         setFilteredYear(selectedYear);
         console.log(filteredYear);
     }
     return(
         <Card className="expenses">
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} LocationOfExpenditure={props.items[0].LocationOfExpenditure}></ExpenseItem>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} LocationOfExpenditure={props.items[1].LocationOfExpenditure}></ExpenseItem>
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} LocationOfExpenditure={props.items[2].LocationOfExpenditure}></ExpenseItem>
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} LocationOfExpenditure={props.items[3].LocationOfExpenditure}></ExpenseItem>
+            {props.items.map((expense,index) =>{
+                return(
+                    <ExpenseItem 
+                        // key = {expense.id}
+                        key = {index}
+                        title = {expense.title} 
+                        amount = {expense.amount} 
+                        date = {expense.date}  
+                    />
+                ) 
+            })}
         </Card>
         
     )
